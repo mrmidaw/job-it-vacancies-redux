d34097c2850cba00558f397e0810197ef84b5b30
Vadim Gavrilov <mr.midaw@gmail.com>
2022-02-24 10:17:17 +0300

    position reducer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { FilterPanel } from 'components/FilterPanel';
 import { JobList } from 'components/JobList';
 import { TheHeader } from 'components/TheHeader';
+import data from './mock/data.json';
+import { addPositions } from './store/positions/position-actions';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(addPositions(data))
+  });
+
   return (
     <>
-     <TheHeader />
-     <div className='container'>
-      <FilterPanel />
-      <JobList />
-     </div>
+      <TheHeader />
+      <div className='container'>
+        <FilterPanel />
+        <JobList />
+      </div>
     </>
   );
 }
